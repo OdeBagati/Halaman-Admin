b6843a5df5161b995fcb6ec5dee84388b77adc8e
I Gede Bagati Kusuma <49517202+OdeBagati@users.noreply.github.com>
2022-07-22 22:58:47 +0800

    Update Transaksi.php

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -2,33 +2,32 @@
 
 namespace App\Controllers;
 
+use stdClass;
+use CodeIgniter\Cookie\Cookie;
+use DateTime;
+
 class Transaksi extends BaseController
 {
     public function index()
     {
-        $lomgin=$this->session->get('lomgin');
+        $lomgin = $this->session->get('lomgin');
 
-        if($lomgin != null)
-        {
-            $url = 'http://128.199.131.109:3000/api/userdata/transaction_history';
-            $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFhejQ3Iiwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE2NTgwNTAzMTl9.a6KnaDbuy5qlLFWkK8EkwfzrBo7Zir5Ycw4SaPJrMwA';
+        if ($lomgin != null) {
+            $url = 'http://128.199.78.209:3000/api/userdata/transaction_history';
+            $token = $lomgin;
             $options = array('http' => array(
                 'method'  => 'GET',
                 'header' => 'Authorization: Bearer ' . $token
             ));
             $context  = stream_context_create($options);
-            $data['title'] = 'transaksi';
             $data['response'] = json_decode(file_get_contents($url, false, $context));
-
             $data['page']  = 'test_table';
             $data['title']  = 'Halaman Transaksi';
 
             // dd($data['response']);
 
             return view('admin', $data);
-        }
-        else
-        {
+        } else {
             return redirect()->to('login');
         }
     }
@@ -38,57 +37,85 @@ class Transaksi extends BaseController
         $this->session->remove('lomgin');
     }
 
-    function dumy()
+    function download()
     {
-        $data['dummy'] = array(
-            '0' => [
-                'tgl_transaksi' => '20/10/2020',
-                'biller' => 'PT XYZ',
-                'trx_id' => '123456',
-                'bill_id' => '123',
-                'bayar' => '69000',
-                'status' => 'berhasil'
-            ],
-            '1' => [
-                'tgl_transaksi' => '20/10/2020',
-                'biller' => 'PT XYZ',
-                'trx_id' => '123456',
-                'bill_id' => '123',
-                'bayar' => '69000',
-                'status' => 'pending'
-            ],
-            '2' => [
-                'tgl_transaksi' => '20/10/2020',
-                'biller' => 'PT XYZ',
-                'trx_id' => '123456',
-                'bill_id' => '123',
-                'bayar' => '69000',
-                'status' => 'gagal'
-            ],
-            '3' => [
-                'tgl_transaksi' => '20/10/2020',
-                'biller' => 'PT XYZ',
-                'trx_id' => '123456',
-                'bill_id' => '123',
-                'bayar' => '69000',
-                'status' => 'berhasil'
-            ],
-            '4' => [
-                'tgl_transaksi' => '20/10/2020',
-                'biller' => 'PT XYZ',
-                'trx_id' => '123456',
-                'bill_id' => '123',
-                'bayar' => '69000',
-                'status' => 'pending'
-            ],
-            '5' => [
-                'tgl_transaksi' => '20/10/2020',
-                'biller' => 'PT XYZ',
-                'trx_id' => '123456',
-                'bill_id' => '123',
-                'bayar' => '69000',
-                'status' => 'gagal'
-            ]
-        );
+        $lomgin = $this->session->get('lomgin');
+
+        if($this->request->getMethod('post'))
+        {
+            $rules = [
+                'date' => [
+                    'rules' => 'required',
+                    'errors'=> [
+                        'required'  => 'Order date is required'
+                    ]
+                ]
+            ];
+
+            if($this->validate($rules))
+            {
+                $dateSelected = $this->request->getPost('date');
+
+                $url = 'http://128.199.78.209:3000/api/admin/transaction_date_no_paging/' . $dateSelected;
+                // $url = 'http://128.199.78.209:3000/api/admin/transaction_date_no_paging/2022-07-21';
+                // $url = 'http://128.199.131.109:3000/api/userdata/transaction_history';
+                $token = $lomgin;
+
+                $options = array('http' => array(
+                    'method'  => 'GET',
+                    'header' => 'Authorization: Bearer ' . $token
+                ));
+                $context  = stream_context_create($options);
+                $response = json_decode(file_get_contents($url, false, $context));
+
+                $data = 'Tanggal Trx' . ' | ' . 'Trx Id' . ' | ' . "BillerId" . ' | ' . 'Period' . ' | ' . 'Biller' . ' | ' . 'Amount' . ' | ' . 'Penalty' . ' | ' . 'Total Amount' . ' | ' . 'Fee' . ' | ' . 'Total Bayar' . ' | ' . "\n";
+                foreach ($response as $trx_list => $transaksi) {
+                    $jsonData = json_decode($transaksi->data);
+                    $data .=  $transaksi->ts . ' | ' . $transaksi->trx_id . ' | ' . $transaksi->product_code . ' | ' . '    -   ' . ' | ' . $transaksi->trx_type . ' | ' . $jsonData->pricing->price . ' | ' . '    -   ' . ' | ' . $jsonData->pricing->price . ' | ' . '   -   ' . ' | ' . $transaksi->amount . ' | ' . "\n";
+                }
+
+                // $data = 'Here is some text!';
+                $name = 'formatreport.txt';
+                return $this->response->download($name, $data);
+                return redirect()->back();
+            }
+            else
+            {
+                echo "kasi tanggal dong";
+            }
+        }
+        else
+        {
+            echo "belum ada method post gan";
+        }   
+    }
+
+    function changeStatus()
+    {
+        $lomgin = $this->session->get('lomgin');
+
+        $ubahStatus = new stdClass();
+        $ubahStatus->trx_id = intval($this->request->getVar('id'));
+        $ubahStatus->amount = intval($this->request->getVar('amount'));
+
+        dd($ubahStatus);
+
+        // $ubahStatus->status = 'reject';
+
+        $url = 'http://128.199.78.209:3000/api/admin/payment_reject';
+        $token = $lomgin;
+        $options = array('http' => array(
+            'method'  => 'POST',
+            'header' => array(
+                "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10",
+                "Content-type: application/json",
+                "Authorization: Bearer " . $token
+            ),
+            'content' => json_encode($ubahStatus)
+        ));
+        $context  = stream_context_create($options);
+        $response = json_decode(file_get_contents($url, false, $context));
+
+        dd($response);
     }
 }
