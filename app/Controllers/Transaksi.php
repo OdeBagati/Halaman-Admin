e59a63e4196766d0b23f623798a4d9c648e32d5f
My Computer <theprastdije@gmail.com>
2022-07-22 17:37:06 +0800

    update url dan token

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -35,14 +35,18 @@ class Transaksi extends BaseController
 
     function download()
     {
-        $url = 'http://128.199.131.109:3000/api/userdata/transaction_history';
-        $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFhejQ3Iiwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE2NTgwNTAzMTl9.a6KnaDbuy5qlLFWkK8EkwfzrBo7Zir5Ycw4SaPJrMwA';
+        $url = 'http://128.199.131.109:3000/api/admin/transaction_date_no_paging/' . date('Y-m-d');
+        // $url = 'http://128.199.78.209:3000/api/admin/transaction_date_no_paging/2022-07-21';
+        // $url = 'http://128.199.131.109:3000/api/userdata/transaction_history';
+        $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFhejQ3Iiwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE2NTg0ODA2NTd9.B1xcTwz3SU_pieAD0_EmemGilsGJdsgralQzeXkbC3M';
+
         $options = array('http' => array(
             'method'  => 'GET',
             'header' => 'Authorization: Bearer ' . $token
         ));
         $context  = stream_context_create($options);
         $response = json_decode(file_get_contents($url, false, $context));
+        // dd($url, $response);
 
         $data = 'Tanggal Trx' . ' | ' . 'Trx Id' . ' | ' . "BillerId" . ' | ' . 'Period' . ' | ' . 'Biller' . ' | ' . 'Amount' . ' | ' . 'Penalty' . ' | ' . 'Total Amount' . ' | ' . 'Fee' . ' | ' . 'Total Bayar' . ' | ' . "\n";
         foreach ($response as $trx_list => $transaksi) {
