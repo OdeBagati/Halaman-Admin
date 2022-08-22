acfd660dd063f493c8f2b18eee610a318ec0420b
I Gede Bagati Kusuma <49517202+OdeBagati@users.noreply.github.com>
2022-08-22 19:55:31 +0800

    Update Transaksi.php

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -117,10 +117,7 @@ class Transaksi extends BaseController
 
                 $data = 'Tanggal Trx' . ' | ' . 'Trx Id' . ' | ' . "BillerId" . ' | ' . 'Period' . ' | ' . 'Biller' . ' | ' . 'Amount' . ' | ' . 'Penalty' . ' | ' . 'Total Amount' . ' | ' . 'Fee' . ' | ' . 'Total Bayar' . ' | ' . "\n";
                 foreach ($hasil as $trx_list => $transaksi) {
-                    // dd($transaksi->data);
                     $jsonData = json_decode($transaksi->data);
-                    // dd($jsonData->pricing);
-                    $harga = $jsonData->pricing;
                     $data .=  $transaksi->ts . ' | ' . $transaksi->trx_id . ' | ' . $transaksi->product_code . ' | ' . '    -   ' . ' | ' . $transaksi->trx_type . ' | ' . $jsonData->pricing->price . ' | ' . '    -   ' . ' | ' . $jsonData->pricing->price . ' | ' . '   -   ' . ' | ' . $transaksi->amount . ' | ' . "\n";
                 }
 
@@ -170,5 +167,28 @@ class Transaksi extends BaseController
 
         // dd($response);
         return redirect()->back();
+    }
+
+    function detail($trx_id)
+    {
+        $lomgin = $this->session->get('lomgin');
+
+        if ($lomgin != null) {
+            $url = 'http://128.199.78.209:3000/api/payment/status/'.$trx_id;
+            $token = $lomgin;
+            $options = array('http' => array(
+                'method'  => 'GET',
+                'header' => 'Authorization: Bearer ' . $token
+            ));
+            $context  = stream_context_create($options);
+            $data['response'] = json_decode(file_get_contents($url, false, $context));
+            $data['dataTransaksi'] = $data['response']->data;
+            $data['page']  = 'test_table';
+            $data['title']  = 'Halaman Transaksi';
+
+            dd($data['response']);
+        } else {
+            return redirect()->to('login');
+        }
     }
 }
