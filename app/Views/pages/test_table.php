aa524d110fb0fccafc1f0e217876458e3b3753c9
I Gede Bagati Kusuma <49517202+OdeBagati@users.noreply.github.com>
2022-10-11 22:40:48 +0800

    Update transaksi paging

--- app/Views/pages/test_table.php
+++ app/Views/pages/test_table.php
@@ -64,24 +64,24 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
+                            <th>TrxID</th>
                             <th>Tgl. Transaksi</th>
                             <th>Biller</th>
-                            <th>TrxID</th>
-                            <th>BillID</th>
+                            <th>Metode Pembayaran</th>
                             <th>Total Bayar</th>
-                            <th>Detail</th>
+                            <!-- <th>Detail</th> -->
                             <th>Status</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
                     <tfoot>
                         <tr>
+                            <th>TrxID</th>
                             <th>Tgl. Transaksi</th>
                             <th>Biller</th>
-                            <th>TrxID</th>
-                            <th>BillID</th>
+                            <th>Metode Pembayaran</th>
                             <th>Total Bayar</th>
-                            <th>Detail</th>
+                            <!-- <th>Detail</th> -->
                             <th>Status</th>
                             <th>Aksi</th>
                         </tr>
@@ -89,28 +89,31 @@
                     <tbody>
                         <?php foreach ($dataTransaksi as $key => $transaksi) : ?>
                             <tr>
+                                <td><?= $transaksi->trx_id; ?></td>
                                 <td><?= date('d-m-Y H:i:s',strtotime($transaksi->ts)); ?></td>
                                 <td><?= strtoupper(str_replace("_"," ",$transaksi->trx_type)); ?></td>
-                                <td><?= $transaksi->trx_id; ?></td>
-                                <td><?= $transaksi->product_code; ?></td>
+                                <td><?= $transaksi->channel; ?></td>
                                 <td>Rp. <?= number_format($transaksi->amount, 0, ',', '.'); ?></td>
-                                <td>
+                                <?php
+                                /*<td>
                                     <div class="row">
                                         <div class="col-12">
                                             <a class="btn btn-info btn-sm" href="<?= base_url(); ?>/detail-transaksi/<?= $transaksi->trx_id; ?>"><i class="fas fa-fw fa-eye"></i> Detail</a>
                                         </div>
                                     </div>
-                                </td>
+                                </td>*/
+                                ?>
                                 <td>
-                                    <?php if ($transaksi->status == 'rejected' || $transaksi->status == 'failed') : ?>
+                                
+                                    <?php if ($transaksi->status == 'rejected' || $transaksi->status == 'failed' || $transaksi->status == 'expired') : ?>
                                         <span class="badge badge-danger"><?= $transaksi->status; ?></span>
-                                    <?php elseif ($transaksi->status == 'unpaid') : ?>
+                                    <?php elseif ($transaksi->status == 'unpaid' || $transaksi->status == 'pending') : ?>
                                         <span class="badge badge-warning"><?= $transaksi->status; ?></span>
                                     <?php else : ?>
                                         <span class="badge badge-success"><?= $transaksi->status; ?></span>
                                     <?php endif; ?>
                                 </td>
-                                <?php if ($transaksi->status != 'rejected' && $transaksi->status != 'done' && $transaksi->status != 'failed') : ?>
+                                <?php if ($transaksi->status != 'rejected' && $transaksi->status != 'done' && $transaksi->status != 'failed' && $transaksi->status != 'expired') : ?>
                                     <td>
                                         <form action="changeStatus/<?= $transaksi->trx_id; ?>" id="ubahStatus" method="post">
                                             <?= csrf_field(); ?>
@@ -126,6 +129,29 @@
                         <?php endforeach; ?>
                     </tbody>
                 </table>
+            </div>
+            <div class="row m-4 text-center">
+                <div class="col-12">
+                    <p>Page Transaksi</p>
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1">Previous</a>
+                            </li>
+                            <?php
+                            $i=1;
+                            for($i=1; $i<=$total_pages; $i++)
+                            { ?>
+                                <li class="page-item"><a class="page-link" href="<?= base_url(); ?>/transaksi/<?= $i; ?>"><?= $i; ?></a></li>
+                                <?php
+                            }
+                            ?>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
